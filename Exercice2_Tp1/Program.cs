f69c5447f47fbbec85edc49979a74fc3a9c9727b
Ayoubouladmaalem <ouladmaalem.ayoub@etu.uae.ac.ma>
2025-03-14 04:45:03 +0000

    push exos

---
+++ Exercice2_Tp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace GestionProjet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Projet monProjet = new Projet("PROJ001", "Application de suivi de consommation de café", 12);
+            
+            monProjet.AjouterProgrammeur(new Programmeur(1, "ECHCHRIKI", "IMAD", 205));
+            monProjet.AjouterProgrammeur(new Programmeur(2, "OUAIL", "ELOUAD", 123));
+            monProjet.AjouterProgrammeur(new Programmeur(3, "TAHA", "NAYA", 501));
+            monProjet.AjouterProgrammeur(new Programmeur(4, "MOUAD", "DAHBI", 678));
+
+
+            Console.WriteLine("\n=== Liste initiale des programmeurs ===");
+            monProjet.AfficherListeProgrammeurs();
+
+            monProjet.AjouterConsommationCafe(1, 1, 4);
+            monProjet.AjouterConsommationCafe(2, 1, 6);
+            monProjet.AjouterConsommationCafe(3, 1, 6);
+            monProjet.AjouterConsommationCafe(4, 1, 1);
+
+
+            Console.WriteLine("\n=== Consommation totale par semaine ===");
+            monProjet.AfficherConsommationTotaleSemaine(1);
+        }
+    }
+}
---
+++ TP4/Program.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using AbsenceManagement.Data;
+using Microsoft.EntityFrameworkCore.SqlServer;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // Supprimez cette ligne pour désactiver HTTPS
+    // app.UseHsts();
+}
+
+// Supprimez ou commentez cette ligne pour désactiver la redirection HTTPS
+// app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
